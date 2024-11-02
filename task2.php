61d6682b19f910be46c32a5cc3e560bdea22b686
Praxidike93 <000865347@mohawkcollege.ca>
2024-11-01 21:39:48 -0400

    Update task2.php

--- task2.php
+++ task2.php
@@ -1,5 +1,5 @@
 <?php
 
-echo 'assignment 3 Part B Task 2 by Zach Kool, uploaded from a GitHub repository!';
+echo 'assignment 3 Part B Task 2 by Zach Kool, uploaded from a GitHub repository! I added this to demonstrate pushing from GitHub!';
 
 ?>
