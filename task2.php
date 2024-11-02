daf48e17860fdfe0a1f674647fd4d6359ca9f38b
Zach Kool <zachpwkool@gmail.com>
2024-11-01 21:51:22 -0400

    updated task2

--- task2.php
+++ task2.php
@@ -1,5 +1,5 @@
 <?php
 
-echo 'assignment 3 Part B Task 2 by Zach Kool, uploaded from a GitHub repository! I added this to demonstrate pushing from GitHub!';
+echo 'assignment 3 Part B Task 2 by Zach Kool, uploaded from a GitHub repository via remote access!';
 
 ?>
